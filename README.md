3b02af61de5c53dd5e098a369cbde2db8d560365
ThomasMeissnerDS <meissnercorporation@gmx.de>
2022-12-29 08:43:41 +0100

    First commit with PSOD class and example notebook

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pseudo-supervised outlier detection
+
+> A highly performant alternative to purely unsupervised approaches.
+
+PSOD uses supervised methods to identify outliers in unsupervised contexts. It offers higher accuracy for outliers
+with top scores than other models while keeping comparable performance on the whole dataset.
+[See the full article](https://medium.com/@thomasmeissnerds)
---
+++ outlier-detection/psod.py
@@ -0,0 +1,185 @@
+import gc
+from typing import Dict, List, Union, Literal
+
+import numpy as np
+import pandas as pd
+from category_encoders import TargetEncoder
+from sklearn.linear_model import LinearRegression
+from tqdm import tqdm
+
+
+class PSOD:
+    def __init__(
+            self,
+            n_jobs=-1,
+            cat_columns=None,
+            min_cols_chosen: float = 0.5,
+            max_cols_chosen: float = 1.0,
+            stdevs_to_outlier: float = 1.96,
+            log_transform: bool = True,
+            random_seed: int = 1,
+            flag_outlier_on: Literal["low end", "both ends", "high end"] = "both ends"
+    ):
+        self.cat_columns = cat_columns
+        self.cat_encoders: Dict[Union[str, int, float], TargetEncoder] = {}
+        self.regressors: Dict[Union[str, int, float], LinearRegression] = {}
+        self.n_jobs = n_jobs
+        self.scores = None
+        self.outlier_classes = None
+        self.min_cols_chosen = min_cols_chosen
+        self.max_cols_chosen = max_cols_chosen
+        self.chosen_columns: List[list] = []
+        self.stdevs_to_outlier = stdevs_to_outlier
+        self.log_transform = log_transform
+        self.flag_outlier_on = flag_outlier_on
+        self.random_seed = random_seed
+        self.random_generator = np.random.default_rng(self.random_seed)
+
+        if self.max_cols_chosen > 1.0:
+            raise ValueError("Param max_cols_chosen cannot be higher than 1.")
+
+        if self.min_cols_chosen <= 0:
+            raise ValueError("Param min_cols_chosen must be higher than 0.")
+
+        if self.flag_outlier_on not in ["low end", "both ends", "high end"]:
+            raise ValueError('Param flag_outlier_on must be any of ["low end", "both ends", "high end"].')
+
+    def get_range_cols(self, df):
+        len_cols = len(df.columns) - 1  # taking out the "target" column
+        self.min_cols_chosen: int = max(int(len_cols * self.min_cols_chosen), 1)
+        self.max_cols_chosen: int = min(int(len_cols * self.max_cols_chosen), len_cols)
+
+    def chose_random_columns(self, df) -> list:
+        """
+        Select random columns.
+
+        Randomize number of columns to chose from as well as the columns chosen.
+        :return: list object with chosen column names
+        """
+        nb_cols: int = self.random_generator.choice(
+            np.arange(self.min_cols_chosen, self.max_cols_chosen) + 1, 1, replace=False
+        )
+        return self.random_generator.choice(df.columns, nb_cols, replace=False).tolist()
+
+    def col_intersection(self, lst1, lst2) -> list:
+        chosen_cat_cols = [value for value in lst1 if value in lst2]
+        return chosen_cat_cols
+
+    def make_outlier_classes(self, df_scores: pd.DataFrame):
+        mean_score = df_scores["anomaly"].mean()
+        std_score = df_scores["anomaly"].std()
+
+        if self.flag_outlier_on == "both ends":
+            conditions = [
+                df_scores["anomaly"] < mean_score - self.stdevs_to_outlier * std_score,
+                df_scores["anomaly"] > mean_score + self.stdevs_to_outlier * std_score
+            ]
+        elif self.flag_outlier_on == "low end":
+            conditions = [
+                df_scores["anomaly"] < mean_score - self.stdevs_to_outlier * std_score
+            ]
+        elif self.flag_outlier_on == "high end":
+            conditions = [
+                df_scores["anomaly"] > mean_score + self.stdevs_to_outlier * std_score
+            ]
+        else:
+            raise ValueError('Param flag_outlier_on must be any of ["low end", "both ends", "high end"].')
+
+        choices = [1 for i in conditions]
+        df_scores["anomaly_class"] = np.select(conditions, choices, default=0)
+        self.outlier_classes = df_scores["anomaly_class"]
+        return df_scores["anomaly_class"]
+
+    def drop_cat_columns(self, df_scores: pd.DataFrame) -> pd.DataFrame:
+        if isinstance(self.cat_columns, list):
+            df_scores["anomaly"] = df_scores.drop(self.cat_columns, axis=1).mean(axis=1)
+        else:
+            df_scores["anomaly"] = df_scores.mean(axis=1)
+
+        self.scores = df_scores["anomaly"]
+        return df_scores
+
+    def fit_predict(self, df, return_class=False) -> pd.Series:
+        df_scores = df.copy()
+        self.get_range_cols(df)
+        if isinstance(self.cat_columns, list):
+            loop_cols = df.drop(self.cat_columns, axis=1).columns
+        else:
+            loop_cols = df.columns
+
+        if self.log_transform and isinstance(self.cat_columns, list):
+            df.drop(self.cat_columns, axis=1).loc[:, :] = np.log1p(
+                df.drop(self.cat_columns, axis=1).loc[:, :]
+            )
+
+        for enum, col in tqdm(enumerate(loop_cols), total=len(loop_cols)):
+            self.chosen_columns.append(self.chose_random_columns(df.drop(col, axis=1)))
+            temp_df = df.copy()
+            # encode categorical columns that are in chosen columns
+            if isinstance(self.cat_columns, list):
+                chosen_cat_cols = self.col_intersection(
+                    self.cat_columns, self.chosen_columns[enum]
+                )
+
+            idx = df_scores.sample(frac=1.0, random_state=enum, replace=True).index
+
+            if isinstance(self.cat_columns, list):
+                enc = TargetEncoder(cols=chosen_cat_cols)
+                temp_df.loc[:, chosen_cat_cols] = enc.fit_transform(
+                    df.loc[:, chosen_cat_cols].iloc[idx].reset_index(drop=True),
+                    df.loc[:, col].iloc[idx].reset_index(drop=True),
+                )
+
+            reg = LinearRegression(n_jobs=self.n_jobs).fit(
+                temp_df.loc[:, self.chosen_columns[enum]].iloc[idx],
+                temp_df[col].iloc[idx],
+            )
+            df_scores[col] = reg.predict(temp_df.loc[:, self.chosen_columns[enum]])
+            df_scores[col] = abs(temp_df[col] - df_scores[col])
+            self.regressors[col] = reg
+            if isinstance(self.cat_columns, list):
+                self.cat_encoders[col] = enc
+            del temp_df
+            _ = gc.collect()
+
+        df_scores = self.drop_cat_columns(df_scores)
+
+        if return_class:
+            return self.make_outlier_classes(df_scores)
+        else:
+            return df_scores["anomaly"]
+
+    def predict(self, df, return_class=False) -> pd.Series:
+        df_scores = df.copy()
+
+        if self.log_transform:
+            df_scores.drop(self.cat_columns, axis=1).loc[:, :] = np.log1p(
+                df_scores.drop(self.cat_columns, axis=1).loc[:, :]
+            )
+
+        if isinstance(self.cat_columns, list) and isinstance(self.cat_columns, list):
+            loop_cols = df.drop(self.cat_columns, axis=1).columns
+        else:
+            loop_cols = df.columns
+
+        for enum, col in tqdm(enumerate(loop_cols)):
+            temp_df = df
+            chosen_cat_cols = self.col_intersection(
+                self.cat_columns, self.chosen_columns[enum]
+            )
+            if isinstance(self.cat_columns, list):
+                enc = self.cat_encoders[col]
+                temp_df[chosen_cat_cols] = enc.transform(df[chosen_cat_cols])
+
+            reg = self.regressors[col]
+
+            df_scores[col] = reg.predict(df[self.chosen_columns[enum]])
+            df_scores[col] = abs(df[col] - df_scores[col])
+            self.regressors[col] = reg
+
+        df_scores = self.drop_cat_columns(df_scores)
+
+        if return_class:
+            return self.make_outlier_classes(df_scores)
+        else:
+            return df_scores["anomaly"]
